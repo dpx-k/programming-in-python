f8d6839d6c654b6613c28fb452fa4b63885c0993
dpx-k <deepakkrishna2206@gmail.com>
2025-02-12 19:13:38 +0530

    added the speed method to increase or decrease speed

--- TurtleGraphics/spiral.py
+++ TurtleGraphics/spiral.py
@@ -6,7 +6,9 @@ miranda = turtle.Turtle()
 distance = 10
 angle = 90 
 
-for _ in range(40): 
+miranda.speed(0)
+
+for _ in range(150): 
     miranda.left(angle)
     miranda.forward(distance)
     distance += 10 

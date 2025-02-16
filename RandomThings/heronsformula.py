55967e41911b067189c862b0067379e7c54fc3a1
dpx-x <deepakkrishna2206@gmail.com>
2025-02-16 17:49:36 +0530

    Added a new folder

---
+++ RandomThings/heronsformula.py
@@ -0,0 +1,7 @@
+# calculate the area of a triangle using heron's formula 
+# heron's formula calculates the area of a triangle using only the sides of the triangle
+
+def take_input(): 
+    a = float(input("Enter Side A: "))
+    b = float(input("Enter Side B: "))
+    c = float(input("Enter Side C: "))

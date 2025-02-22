6ec91e9799de669b00e9b368fd24a4b6e123bc51
dpx-k <deepakkrishna2206@gmail.com>
2025-02-22 19:10:14 +0530

    renamed random things to exercises, learnt new things

---
+++ 7.Strings/concatenation.py
@@ -0,0 +1,9 @@
+# capitalise the name terry (apply the capitalise() function)
+
+name='TERRY'
+color = 'RED'
+msg = '[' + name + '] loves the color ' + color.lower() + '!'
+msg1 = f'[{name.capitalize()}] loves the color {color.lower()}!'
+print(msg)
+print(msg1)
+
